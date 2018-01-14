c03a885241971b686c05b7233c1142e77a7b7340
Zheng Fu <zf38@cornell.edu>
2018-01-14 14:34:17 -0600

    added back welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'courses/index'
 
+  get 'welcome/index'
+
   root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
