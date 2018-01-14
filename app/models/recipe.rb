75a57ea99f0552d646585341a870eda950186c70
Zheng Fu <zf38@cornell.edu>
2018-01-14 14:32:10 -0600

    added recipe finder

---
+++ app/models/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe
+  include HTTParty
+
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV['FOOD2FORK_KEY']
+  format :json
+
+  def self.for(term)
+    get('/search', query: { q: term })['recipes']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipe/index'
+
   get 'courses/index'
 
-  root 'welcome#index'
+  root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
